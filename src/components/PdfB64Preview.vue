bfe54f93421a6e0e68e4e837d111814328c57f10
Renzo Loui Chávez Caycho <rlchavezc@outlook.com>
2023-02-12 18:02:03 -0500

    Added preview media files

---
+++ src/components/PdfB64Preview.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ratio ratio-16x9">
+        <iframe :src="fileUrl" title="Archivo PDF B64" allowfullscreen></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PdfB64Preview',
+    props: {
+        fileBase64: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        fileUrl() {
+            return `data:application/pdf;base64,${this.fileBase64}`
+        }
+    }
+}
+</script>
